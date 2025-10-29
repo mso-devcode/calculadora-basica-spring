e9f471e2e9daed6e59bcc24365504b8f08489ad8
mso-devcode <mso.devcode@gmail.com>
2025-10-29 08:14:34 -0300

    chorus: first commit

---
+++ src/test/java/com/mso/api_ai/StartupTests.java
@@ -0,0 +1,13 @@
+package com.mso.api_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
